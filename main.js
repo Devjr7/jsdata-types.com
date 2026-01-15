8020ac530256819a2388774fff66dbb9371cfe43
Devjr7 <devudevansh22@gmail.com>
2026-01-15 10:35:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+x = 2
+y = 4
+
+x + y
+
+//ex of int
+console.log(7)
+console.log(19)
+console.log(90)
+//ex of float
+console.log(3.14)
+console.log(5.2)
+//ex of string
+console.log("ilove to code!")
